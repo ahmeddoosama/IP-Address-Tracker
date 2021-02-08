2404c680af0f16f2ed938ee3c6813050a12c96df
Ahmed Osama <ahmedosamabedawy@gmail.com>
2021-02-09 00:57:17 +0200

    fix some issue in map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,4 @@
-let map,
-    marker,
-    showMap = document.getElementById('map');
+let map,marker
 
 function setMap({
     lat,
@@ -9,26 +7,28 @@ function setMap({
     if (map) {
         map.remove()
         map = undefined
-        showMap.innerHTML = '';
+        document.getElementById('map').innerHTML = '';
     }
 
     map = L.map('map').setView([lat, lng], 7)
 
     const titleAttribution = `
-        <a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a>
+        <a href="https://www.maptiler.com/copyright/" target="_blank">
+            &copy; MapTiler
+        </a>
         <a href="https://www.openstreetmap.org/copyright" target="_blank">
             &copy; OpenStreetMap contributors
         </a>`;
 
-    L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=pk.eyJ1IjoiYWhtZWRiZWRhd3kiLCJhIjoiY2trd3V3ZHY5MmU0MzJ2czFucXN1enRiNiJ9.fvZIGW1qAXigmtszbYkg5g', {
-        attribution: tileAttribution
+    L.tileLayer('https://api.maptiler.com/maps/hybrid/{z}/{x}/{y}.jpg?key=09L75MTDaCzv2tDie8A4', {
+        attribution: titleAttribution
     }).addTo(map);
 
-    let markerIcon = L.icon({
-		iconUrl: '../images/icons/icon-location.svg'
-	})
+    // let markerIcon = L.icon({
+	// 	iconUrl: '../images/icons/icon-location.svg'
+	// })
 
-    marker = L.marker([lat, lng], {icon: markerIcon}).addTo(map);
+    marker = L.marker([lat, lng]).addTo(map);
 }
 
 export default setMap;
