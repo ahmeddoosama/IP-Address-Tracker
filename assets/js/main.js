a76ab3052d5f9229a5e2649e266a8a5900bf7c40
Ahmed Osama <ahmedosamabedawy@gmail.com>
2021-02-08 23:55:52 +0200

    add setUpEverything function in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,26 @@
 import fetchData from './fetch';
 import setMap from './map';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+})
+
+async function setUpEverything(queryType, queryValue) {
+    
+    const data = await fetchData(queryType, queryValue)
+
+    displayUserInfo(data)
+    setMap(data.location)
+}
+
+function displayUserInfo({ip, location, isp}) {
+    const [IP, Location, TimeZone, ISP] = document.getElementsByClassName('data-value')
+
+    IP.innerText = ip;
+	Location.innerText = `${location.region}, ${location.country} ${location.postalCode}`;
+	TimeZone.innerText = 'UTC ' + location.timezone;
+	ISP.innerText = isp;
+}
+
+export {setUpEverything}
